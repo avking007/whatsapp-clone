9f94f0f45b6aea2278a3106c7d203a7c0a21e406
Anish Varshney <avanvarshney@gmail.com>
2020-12-08 16:51:05 +0530

    +user route

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+// middleware
+app.use(express.json({ extended: false }));
+
+// db connect
+connectDB();
+
+// define port
+const PORT = process.env.PORT || 5000;
+
+// routes
+app.use('/user/', require('./routes/user'));
+
+app.listen(PORT, () => console.log('working'));
