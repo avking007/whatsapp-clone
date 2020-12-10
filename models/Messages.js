1e70eda9a601c2ccdaf2278eee22ee19b1049722
Anish Varshney <avanvarshney@gmail.com>
2020-12-10 14:12:16 +0530

    + message model

---
+++ models/Messages.js
@@ -0,0 +1,26 @@
+const mongo = require('mongoose');
+
+const messageSchema = mongo.Schema({
+  room: {
+    type: mongo.Schema.Types.ObjectId,
+    ref: 'rooms',
+  },
+  msg_contents: [
+    {
+      user: {
+        type: String,
+        required: true,
+      },
+      message: {
+        type: String,
+        required: true,
+      },
+      date: {
+        type: Date,
+        default: Date.now(),
+      },
+    },
+  ],
+});
+
+module.exports = Message = mongo.model('messages', messageSchema);
