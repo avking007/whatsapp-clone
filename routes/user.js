690a7fae7f3517d1b01e7cb5f6238ad47f28f9e9
Anish Varshney <avanvarshney@gmail.com>
2020-12-10 23:51:32 +0530

    +google login api

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,13 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const config = require('../config/default');
 const auth = require('../middleware/auth');
-// route  api/signup
+const { OAuth2Client } = require('google-auth-library');
+
+const client = new OAuth2Client(
+  '699625891152-llvk17ddp5otqeqc1076gcid8ipidtsr.apps.googleusercontent.com'
+);
+
+// route  user/signup
 // access public
 // desc  signup user
 router.post(
@@ -63,7 +69,7 @@ router.post(
   }
 );
 
-// route api/add_img
+// route user/add_img
 // access private
 // add dp
 router.put(
@@ -87,7 +93,7 @@ router.put(
   }
 );
 
-// route api/login
+// route user/login
 // access public
 // desc login
 router.post(
@@ -131,5 +137,74 @@ router.post(
     }
   }
 );
+
+// route user/googlelogin
+// access public
+// desc google login/signup
+router.post('/googlelogin', async (req, res) => {
+  const { tokenId, img } = req.body;
+  try {
+    const match = await client.verifyIdToken({
+      idToken: tokenId,
+      audience:
+        '699625891152-llvk17ddp5otqeqc1076gcid8ipidtsr.apps.googleusercontent.com',
+    });
+    const { email_verified, name, email } = match.payload;
+    if (email_verified) {
+      let user = await User.findOne({ email }).select('-password');
+      if (!user) {
+        // create user and send token
+        let newMem = { email, name, image: img };
+        const password = '`cFt`7//E' + email + 'Z^eeXq=h#';
+        const salt = await hasher.genSalt(10);
+        newMem.password = await hasher.hash(password, salt);
+
+        const newUser = new User(newMem);
+        await newUser.save();
+        // jwt
+        const payload = {
+          user: {
+            id: newUser.id,
+          },
+        };
+        jwt.sign(
+          payload,
+          config.jwtKey,
+          {
+            expiresIn: '10h',
+          },
+          (err, token) => {
+            if (err) throw err;
+            return res.json({
+              token,
+              user: { _id: newUser.id, name, email, image: img },
+            });
+          }
+        );
+      } else {
+        // send token
+        const payload = {
+          user: {
+            id: user._id,
+          },
+        };
+
+        jwt.sign(
+          payload,
+          config.jwtKey,
+          {
+            expiresIn: '10h',
+          },
+          (err, token) => {
+            if (err) throw err;
+            return res.json({ token, user });
+          }
+        );
+      }
+    }
+  } catch (error) {
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
