e8df70abdeadf486d8c84b08f3652f04c49f2cce
Anish Varshney <avanvarshney@gmail.com>
2020-12-10 16:46:24 +0530

    +pusher

--- config/db.js
+++ config/db.js
@@ -1,5 +1,14 @@
 const mongo = require('mongoose');
 const config = require('./default');
+const Pusher = require('pusher');
+
+const pusher = new Pusher({
+  appId: '1120371',
+  key: '59140e79370d6491a60f',
+  secret: '10a87abd59fd11b13522',
+  cluster: 'ap2',
+  useTLS: true,
+});
 
 const connect = async () => {
   try {
@@ -9,7 +18,32 @@ const connect = async () => {
       useFindAndModify: false,
       useCreateIndex: true,
     });
-    console.log('db connected');
+    // pusher
+    const db = mongo.connection;
+    db.once('open', () => {
+      console.log('db connected');
+      const messages = db.collection('messages');
+      const changestream = messages.watch();
+
+      changestream.on('change', (change) => {
+        // console.log(change);
+        if (change.operationType == 'update') {
+          const msg_model = change.documentKey;
+          const changed = change.updateDescription.updatedFields;
+          let data = {};
+          for (let key in changed) {
+            if (key.startsWith('_')) continue;
+            else {
+              data['new_msg'] = changed[key];
+            }
+          }
+          pusher.trigger('messages', 'updated', {
+            data: data['new_msg'],
+            msg_model,
+          });
+        }
+      });
+    });
   } catch (error) {
     console.log(error);
   }
