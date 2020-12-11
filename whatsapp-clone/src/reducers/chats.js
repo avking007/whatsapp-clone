45c55a34d9d162164505ee0c99049dbdd4bd6148
Anish Varshney <avanvarshney@gmail.com>
2020-12-11 20:32:30 +0530

    +load room

--- whatsapp-clone/src/reducers/chats.js
+++ whatsapp-clone/src/reducers/chats.js
@@ -1,19 +1,41 @@
-import { CLEAR_ROOM, ROOM_ERROR } from '../actions/types';
+import { CLEAR_ROOM, ROOM_ERROR, ROOM_LOADED } from '../actions/types';
 
 const initState = {
   room: null,
   loading: true,
-  error: {},
+  error: [],
+  messages: [],
+  msg_model: null,
 };
 export default function chats(state = initState, action) {
   const { type, payload } = action;
   switch (type) {
     case CLEAR_ROOM:
-      return { ...state, loading: true, room: null };
+      return {
+        ...state,
+        loading: true,
+        room: null,
+        messages: [],
+        msg_model: null,
+        error: [],
+      };
 
     case ROOM_ERROR:
-      return { ...state, loading: false, error: payload };
-
+      return {
+        ...state,
+        loading: true,
+        error: payload,
+        messages: [],
+        msg_model: null,
+      };
+    case ROOM_LOADED:
+      return {
+        ...state,
+        room: payload.room,
+        messages: payload.msg_contents,
+        loading: false,
+        msg_model: payload.msg_model,
+      };
     default:
       return state;
   }
