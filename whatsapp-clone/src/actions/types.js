1d25b10c2d120afdbcd5286e82a12b041737b73a
Anish Varshney <avanvarshney@gmail.com>
2020-12-11 18:40:36 +0530

    +types

--- whatsapp-clone/src/actions/types.js
+++ whatsapp-clone/src/actions/types.js
@@ -1,12 +1,11 @@
+// user auth
 export const AUTH_ERROR = 'AUTH_ERROR';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
-export const REGISTER_FAIL = 'REGISTER_FAIL';
 export const LOGOUT = 'LOGOUT';
-export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const USER_LOADED = 'USER_LOADED';
-export const ROOMS_LOADED = 'ROOMS_LOADED';
+
+// rooms
 export const ROOM_LOADED = 'ROOM_LOADED';
 export const CLEAR_ROOM = 'CLEAR_ROOM';
-export const CLEAR_ALL_ROOMS = 'CLEAR_ALL_ROOMS';
 export const ROOM_ERROR = 'ROOM_ERROR';
