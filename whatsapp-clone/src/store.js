ae8eff117365d1516cb4caa0712f8cd159ca9a23
Anish Varshney <avanvarshney@gmail.com>
2020-12-11 13:17:40 +0530

    +store

---
+++ whatsapp-clone/src/store.js
@@ -0,0 +1,15 @@
+import rootreducer from './reducers/root';
+import { createStore, applyMiddleware } from 'redux';
+import composeWithDevTools from 'redux-devtools-extension';
+import { thunk } from 'redux-thunk';
+
+const middleware = [thunk];
+
+const initState = {};
+const store = createStore(
+  rootreducer,
+  initState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
