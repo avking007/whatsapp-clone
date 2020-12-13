e5cc69bb38549d6cc87cd5f9f945779200f65b63
Anish Varshney <avanvarshney@gmail.com>
2020-12-14 02:10:49 +0530

    +navigations

--- whatsapp-clone/src/components/layout/SideBar.js
+++ whatsapp-clone/src/components/layout/SideBar.js
@@ -1,32 +1,76 @@
-import { IconButton, Avatar } from '@material-ui/core';
+import { IconButton, Avatar, Menu, Fade, MenuItem } from '@material-ui/core';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import SearchOutLined from '@material-ui/icons/SearchOutlined';
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import './Sidebar.css';
 import SidebarChat from './SidebarChat';
-import { Redirect } from 'react-router-dom';
+import { Redirect, withRouter } from 'react-router-dom';
+import { logout } from '../../actions/auth';
+
 function SideBar({
   user: {
     isAuth,
-    user: { image, name, participant },
+    user: { image, name, participant, _id },
   },
+  history,
+  logout,
 }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
   if (!isAuth) {
     return <Redirect to='/login' />;
   }
+  const LogoutHandler = () => {
+    logout();
+  };
+
+  const newChatHandler = () => {
+    history.push('/create_room');
+  };
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const SettingClickHandler = () => {
+    history.push(`/${_id}/setting`);
+  };
+
   return (
     <div className='sidebar'>
       <div className='sidebar__header'>
         <Avatar src={image} />
-        <div className='sidebar__headerRight'>
-          <IconButton>
+        <div className='sidebar__headerRights'>
+          <IconButton onClick={newChatHandler}>
             <ChatIcon />
           </IconButton>
-          <IconButton>
+
+          <IconButton
+            aria-controls='fade-menu'
+            aria-haspopup='true'
+            onClick={handleClick}
+          >
             <MoreVertIcon />
           </IconButton>
+
+          <Menu
+            id='fade-menu'
+            anchorEl={anchorEl}
+            keepMounted
+            open={open}
+            onClose={handleClose}
+            TransitionComponent={Fade}
+            getContentAnchorEl={null}
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+            transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+          >
+            <MenuItem onClick={SettingClickHandler}>Settings</MenuItem>
+            <MenuItem onClick={LogoutHandler}>Logout</MenuItem>
+          </Menu>
         </div>
       </div>
       <div className='sidebar__search'>
@@ -53,4 +97,4 @@ const mapper = (state) => ({
   user: state.user,
 });
 
-export default connect(mapper)(SideBar);
+export default connect(mapper, { logout })(withRouter(SideBar));
