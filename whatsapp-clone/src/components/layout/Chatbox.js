0b0c4af2ae2003ec68d6a6e5604a56631a7c2a86
Anish Varshney <avanvarshney@gmail.com>
2020-12-14 02:11:26 +0530

    +navigations

--- whatsapp-clone/src/components/layout/Chatbox.js
+++ whatsapp-clone/src/components/layout/Chatbox.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Chatbox.css';
-import { Avatar, IconButton } from '@material-ui/core';
+import { Avatar, Fade, IconButton, Menu, MenuItem } from '@material-ui/core';
+import { connect } from 'react-redux';
 import {
   AttachFile,
   SearchOutlined,
@@ -8,16 +9,61 @@ import {
   InsertEmoticon,
 } from '@material-ui/icons';
 import Moment from 'react-moment';
-function Chatbox() {
-  return (
+import MessageItem from './MessageItem';
+import { sendMessage } from '../../actions/room';
+import { Redirect, withRouter } from 'react-router-dom';
+import { close_group } from '../../actions/room';
+
+function Chatbox({
+  room,
+  messages,
+  loading,
+  user,
+  sendMessage,
+  isAuth,
+  history,
+  close_group,
+}) {
+  const [mess, setmess] = useState({ user: '', message: '' });
+  const [anchorEl, setanchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleOpen = (event) => {
+    setanchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setanchorEl(null);
+  };
+  const groupCloseHandler = () => {
+    close_group();
+    handleClose();
+  };
+  const GroupSettinghandler = () => {
+    history.push(`/${room._id}/room_setting`);
+  };
+  const changeHandler = (e) => {
+    const sender = user && user.name;
+    if (mess.user) {
+      setmess({ ...mess, message: e.target.value });
+    } else {
+      setmess({ ...mess, message: e.target.value, user: sender });
+    }
+  };
+  const SubmitHandler = (e) => {
+    e.preventDefault();
+    setmess({ ...mess, message: '' });
+    sendMessage(mess, messages._id);
+  };
+  if (!isAuth) {
+    return <Redirect to='/login' />;
+  }
+  return loading ? null : (
     <div className='chatbox'>
       <div className='chatbox__header'>
-        <Avatar />
+        <Avatar src={room.image} />
         <div className='chatbox__headerinfo'>
-          <h3>Room name</h3>
+          <h3>{room.title}</h3>
           <p>
-            Created on{' '}
-            <Moment format='DD/MM/YY'>{new Date().toUTCString()}</Moment>
+            Created on <Moment format='DD/MM/YY'>{room.date}</Moment>
           </p>
         </div>
         <div className='chatbox__headerRight'>
@@ -27,44 +73,68 @@ function Chatbox() {
           <IconButton>
             <AttachFile />
           </IconButton>
-          <IconButton>
+          <IconButton
+            aria-controls='fade-menu'
+            aria-haspopup='true'
+            onClick={handleOpen}
+          >
             <MoreVert />
           </IconButton>
+          <Menu
+            id='fade-menu'
+            anchorEl={anchorEl}
+            keepMounted
+            open={open}
+            onClose={handleClose}
+            TransitionComponent={Fade}
+            getContentAnchorEl={null}
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+            transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+          >
+            <MenuItem onClick={GroupSettinghandler}>Settings</MenuItem>
+            <MenuItem onClick={groupCloseHandler}>Close Group</MenuItem>
+          </Menu>
         </div>
       </div>
       <div className='chatbox__body'>
-        <p className='chat__message'>
-          <span className='chat__name'>Anish</span>
-          This is message
-          <span className='chat__timestamp'>
-            <Moment fromNow>{new Date().toUTCString()}</Moment>
-          </span>
-        </p>
-        <p className='chat__message chat__reciever'>
-          <span className='chat__name'>Anish</span>
-          This is message
-          <span className='chat__timestamp'>
-            <Moment fromNow>{new Date().toUTCString()}</Moment>
-          </span>
-        </p>
-        <p className='chat__message'>
-          <span className='chat__name'>Anish</span>
-          This is message
-          <span className='chat__timestamp'>
-            <Moment fromNow>{new Date().toUTCString()}</Moment>
-          </span>
-        </p>
+        {messages &&
+          messages.msg_contents &&
+          messages.msg_contents.map((mess) => (
+            <MessageItem key={mess._id} msg={mess} />
+          ))}
       </div>
       <div className='chat__footer'>
         <IconButton>
           <InsertEmoticon />
         </IconButton>
-        <form>
-          <input placeholder='Type your message' type='text' />
+        <form
+          onSubmit={(e) => {
+            SubmitHandler(e);
+          }}
+        >
+          <input
+            placeholder='Type your message'
+            type='text'
+            name='message'
+            value={mess.message}
+            onChange={(e) => {
+              changeHandler(e);
+            }}
+          />
         </form>
       </div>
     </div>
   );
 }
 
-export default Chatbox;
+export const mapper = (state) => ({
+  room: state.chats.room,
+  messages: state.chats.msg_model,
+  loading: state.chats.loading,
+  user: state.user.user,
+  isAuth: state.user.isAuth,
+});
+
+export default connect(mapper, { sendMessage, close_group })(
+  withRouter(Chatbox)
+);
