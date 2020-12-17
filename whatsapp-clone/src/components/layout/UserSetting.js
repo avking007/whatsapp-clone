3ab2ac691b5e923aa6c078417215c667cd67e55b
Anish Varshney <avanvarshney@gmail.com>
2020-12-17 21:09:56 +0530

    +user image manually

--- whatsapp-clone/src/components/layout/UserSetting.js
+++ whatsapp-clone/src/components/layout/UserSetting.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import { Avatar, Button, TextField } from '@material-ui/core';
+import React, { useState } from 'react';
+import {
+  Avatar,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  TextField,
+} from '@material-ui/core';
 import { Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { logout } from '../../actions/auth';
+import { logout, add_image } from '../../actions/auth';
 import './userSetting.css';
-function UserSetting({ history, logout, isAuth, user }) {
+
+function UserSetting({ history, logout, isAuth, user, add_image, user_image }) {
+  const [dispImage, setdispImage] = useState('/' + user_image);
+
+  const [dpmenu, setdpmenu] = useState(false);
+  const [image, setimage] = useState({ file: null });
   const goBackHandler = () => {
     history.push('/');
   };
@@ -14,19 +27,33 @@ function UserSetting({ history, logout, isAuth, user }) {
   if (!isAuth) {
     return <Redirect to='/login' />;
   }
+  const dpToggler = () => {
+    setdpmenu(!dpmenu);
+  };
+  const addImage = (file) => {
+    setimage({ file: file });
+  };
+  const dpSubmitHandler = (e) => {
+    // e.preventDefault();
+    if (image.file) {
+      add_image(user._id, image.file);
+      // setdispImage('/' + user_image);
+    }
+    setimage({ file: null });
+    dpToggler();
+  };
 
-  const { name, email, image } = user;
+  const { name, email } = user;
   return (
     <div className='setting'>
       <div className='setting__body'>
         {/* header */}
         <div className='setting__header'>
-          <Avatar src={image} />
+          <Avatar src={dispImage} />
           <div className='setting__headerRight'>
             <TextField
               value={name}
               label='Name'
-              id='standard-read-only-input'
               InputProps={{
                 readOnly: true,
               }}
@@ -34,7 +61,6 @@ function UserSetting({ history, logout, isAuth, user }) {
             <TextField
               value={email}
               label='Email'
-              id='standard-read-only-input'
               InputProps={{
                 readOnly: true,
               }}
@@ -44,7 +70,36 @@ function UserSetting({ history, logout, isAuth, user }) {
         </div>
         {/* settings */}
         <div className='setting__buttons'>
-          <Button>Change DP</Button>
+          <Button onClick={dpToggler}>Change DP</Button>
+          <Dialog
+            open={dpmenu}
+            onClose={dpToggler}
+            aria-labelledby='responsive-dialog-title'
+          >
+            <DialogContent>
+              <DialogContentText>
+                <strong>Upload Image</strong>
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <form onSubmit={dpSubmitHandler} className='image_form'>
+                <TextField
+                  required
+                  name='email'
+                  variant='outlined'
+                  id='outlined-basic'
+                  type='file'
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                  onChange={(e) => {
+                    addImage(e.target.files);
+                  }}
+                />
+                <Button type='submit'>Upload</Button>
+              </form>
+            </DialogActions>
+          </Dialog>
           <Button onClick={logoutHandler}>Logout </Button>
         </div>
       </div>
@@ -54,6 +109,7 @@ function UserSetting({ history, logout, isAuth, user }) {
 const mapper = (state) => ({
   isAuth: state.user.isAuth,
   user: state.user.user,
+  user_image: state.user.image,
 });
 
-export default connect(mapper, { logout })(withRouter(UserSetting));
+export default connect(mapper, { logout, add_image })(withRouter(UserSetting));
