41d95cc910d34039769d0c442691330f345d8586
Anish Varshney <avanvarshney@gmail.com>
2020-12-17 21:11:41 +0530

    +image upload routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+const cors = require('cors');
 // middleware
 app.use(express.json({ extended: false }));
 
@@ -11,7 +12,9 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 
 // routes
+app.use(cors());
 app.use('/user/', require('./routes/user'));
 app.use('/room/', require('./routes/room'));
-
+app.use('/uploads/users/', express.static('./uploads/users'));
+app.use('/uploads/rooms/', express.static('./uploads/rooms'));
 app.listen(PORT, () => console.log('working'));
