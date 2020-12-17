b2336d72ddc2b7e5c31b50c92d7cc22bece80fa8
Anish Varshney <avanvarshney@gmail.com>
2020-12-17 21:03:32 +0530

    +image

--- whatsapp-clone/src/actions/types.js
+++ whatsapp-clone/src/actions/types.js
@@ -4,7 +4,8 @@ export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOGOUT = 'LOGOUT';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const USER_LOADED = 'USER_LOADED';
-
+export const IMAGE_UPLOADED = 'IMAGE_UPLOADED';
+export const IMAGE_UPLOAD_FAIL = 'IMAGE_UPLOAD_FAIL';
 // rooms
 export const ROOM_LOADED = 'ROOM_LOADED';
 export const CLEAR_ROOM = 'CLEAR_ROOM';
@@ -15,3 +16,5 @@ export const ROOM_CREATED = 'ROOM_CREATED';
 export const ROOM_CREATE_FAIL = 'ROOM_CREATE_FAIL';
 export const MEMBER_ADDED = 'MEMBER_ADDED';
 export const MEMBER_ADD_FAIL = 'MEMBER_ADD_FAIL';
+export const GROUP_DP_UPLOAD = 'GROUP_DP_UPLOAD';
+export const GROUP_DP_FAIL = 'GROUP_DP_FAIL';
