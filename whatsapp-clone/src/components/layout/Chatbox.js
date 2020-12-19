ca750d361e395ee521d58930f68cc946e9e3eb76
Anish Varshney <avanvarshney@gmail.com>
2020-12-19 23:11:21 +0530

    +realtime chat

--- whatsapp-clone/src/components/layout/Chatbox.js
+++ whatsapp-clone/src/components/layout/Chatbox.js
@@ -1,6 +1,13 @@
 import React, { useState, useRef, useEffect } from 'react';
 import './Chatbox.css';
-import { Avatar, Fade, IconButton, Menu, MenuItem } from '@material-ui/core';
+import {
+  Avatar,
+  Fade,
+  IconButton,
+  Menu,
+  MenuItem,
+  TextField,
+} from '@material-ui/core';
 import { connect } from 'react-redux';
 import {
   AttachFile,
@@ -12,8 +19,9 @@ import Moment from 'react-moment';
 import MessageItem from './MessageItem';
 import { sendMessage } from '../../actions/room';
 import { Redirect, withRouter } from 'react-router-dom';
-import { close_group } from '../../actions/room';
+import { close_group, new_message } from '../../actions/room';
 
+import Pusher from 'pusher-js';
 function Chatbox({
   room,
   messages,
@@ -22,33 +30,51 @@ function Chatbox({
   sendMessage,
   isAuth,
   history,
+  msgModel,
   close_group,
+  new_message,
 }) {
   const scrollBottom = useRef();
   const scrollToBottom = () => {
     scrollBottom.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
   };
+
   useEffect(() => {
-    if (messages) {
+    if (messages.length > 0) {
       scrollToBottom();
     }
   }, [messages]);
+  // console.log(messages);
+  const [srch_message, setsrch_message] = useState('');
   const [mess, setmess] = useState({ user: '', message: '' });
   const [anchorEl, setanchorEl] = useState(null);
+  const [searchEl, setsearchEl] = useState(null);
   const open = Boolean(anchorEl);
+  const openSrch = Boolean(searchEl);
+
+  // --------
+  // setting menu toggle
   const handleOpen = (event) => {
     setanchorEl(event.currentTarget);
   };
   const handleClose = () => {
     setanchorEl(null);
   };
+  // --------
+
+  // --------
+  // close currently open group
   const groupCloseHandler = () => {
     close_group();
     handleClose();
   };
+  // --------
+
   const GroupSettinghandler = () => {
     history.push(`/${room._id}/room_setting`);
   };
+  // --------
+  // message input handle
   const changeHandler = (e) => {
     const sender = user && user.name;
     if (mess.user) {
@@ -57,14 +83,56 @@ function Chatbox({
       setmess({ ...mess, message: e.target.value, user: sender });
     }
   };
+  // --------
+
+  // ------
+  // send message
   const SubmitHandler = (e) => {
     e.preventDefault();
     setmess({ ...mess, message: '' });
-    sendMessage(mess, messages._id);
+    sendMessage(mess, msgModel._id);
+  };
+
+  useEffect(() => {
+    const pusher = new Pusher('59140e79370d6491a60f', {
+      cluster: 'ap2',
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('updated', function (newMessage) {
+      if (room) {
+        new_message(newMessage, room.messageModel, user._id);
+      }
+    });
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [room, new_message, user]);
+
+  //-------
+
+  // ---
+  // Search message menu toggle
+  const handleSearchOpen = (e) => {
+    setsearchEl(e.currentTarget);
+  };
+  const handleSearchClose = () => {
+    setsearchEl(null);
+  };
+  // ----
+
+  // search message
+  const SearchMessage = () => {};
+
+  // handle search message input
+  const srchMessageChange = (e) => {
+    setsrch_message(e.target.value);
   };
   if (!isAuth) {
     return <Redirect to='/login' />;
   }
+
   return loading ? null : (
     <div className='chatbox'>
       <div className='chatbox__header'>
@@ -76,9 +144,39 @@ function Chatbox({
           </p>
         </div>
         <div className='chatbox__headerRight'>
-          <IconButton>
+          <IconButton
+            aria-controls='fade-menu'
+            aria-haspopup='true'
+            onClick={handleSearchOpen}
+          >
             <SearchOutlined />
           </IconButton>
+          <Menu
+            id='fade-menu'
+            anchorEl={searchEl}
+            keepMounted
+            open={openSrch}
+            onClose={handleSearchClose}
+            getContentAnchorEl={null}
+            TransitionComponent={Fade}
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+            transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+          >
+            <MenuItem>
+              <form onSubmit={SearchMessage}>
+                <TextField
+                  autoComplete='off'
+                  label='Search message'
+                  variant='outlined'
+                  value={srch_message}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  onChange={(e) => srchMessageChange(e)}
+                />
+              </form>
+            </MenuItem>
+          </Menu>
           <IconButton>
             <AttachFile />
           </IconButton>
@@ -107,11 +205,8 @@ function Chatbox({
       </div>
       <div className='chatbox__body hidden_scroll'>
         {messages &&
-          messages.msg_contents &&
-          messages.msg_contents.map((mess) => (
-            <MessageItem key={mess._id} msg={mess} />
-          ))}
-        <div ref={scrollBottom}></div>
+          messages.map((mess) => <MessageItem key={mess._id} msg={mess} />)}
+        <div ref={scrollBottom} />
       </div>
       <div className='chat__footer'>
         <IconButton>
@@ -123,6 +218,7 @@ function Chatbox({
           }}
         >
           <input
+            autoComplete='off'
             placeholder='Type your message'
             type='text'
             name='message'
@@ -139,12 +235,13 @@ function Chatbox({
 
 export const mapper = (state) => ({
   room: state.chats.room,
-  messages: state.chats.msg_model,
+  messages: state.chats.messages,
   loading: state.chats.loading,
   user: state.user.user,
   isAuth: state.user.isAuth,
+  msgModel: state.chats.msg_model,
 });
 
-export default connect(mapper, { sendMessage, close_group })(
+export default connect(mapper, { new_message, sendMessage, close_group })(
   withRouter(Chatbox)
 );
